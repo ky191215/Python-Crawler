1207464a640276f0e04303d2809565fc065fc619
YungYi0202 <cathy@anyday.com.tw>
2019-12-28 14:27:50 +0800

    Last modification

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ from args import get_args
 
 if __name__ == '__main__':
     args = get_args()
-    print(args.start_date, args.end_date)
     crawler = Crawler()
     results = crawler.crawl(args.start_date, args.end_date)
-    print(results)
     
     # DONE: write result to file according to spec
     with open(args.output + '.csv', 'w') as fp:
         for date, title, content in results:
-            #result[0] -> post date, [1] -> title, [2] -> content (url for now)
             title = title.replace('\n',' ').replace('\r','')
             title = title.replace('\"','\"\"')
             content = content.replace('\n',' ').replace('\r','')
