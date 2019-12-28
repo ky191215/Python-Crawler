998be7d6d960caa0d2eac080cbda8bdae2761f71
YungYi0202 <cathy@anyday.com.tw>
2019-12-28 11:04:44 +0800

    Add the features to crawl contents and write files
    Note that :
    1. not sure if the garbled topics and contents are normal or not.
    2. the replace func in main.py is just written by following TA's demo.

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ if __name__ == '__main__':
     results = crawler.crawl(args.start_date, args.end_date)
     print(results)
     
-    # TODO: write result to file according to spec
+    # DONE: write result to file according to spec
     with open(args.output + '.csv', 'w') as fp:
-        #result[0] -> post date, [1] -> title, [2] -> content (url for now)
-        for result in results:
-            # Need modification for output specification afterwards
-            fp.write(result[0]+'\n' + result[1] + '\n' + result[2] + '\n\n')
+        for date, title, content in results:
+            #result[0] -> post date, [1] -> title, [2] -> content (url for now)
+            title = title.replace('\n',' ').replace('\r','')
+            title = title.replace('\"','\"\"')
+            content = content.replace('\n',' ').replace('\r','')
+            content = content.replace('\"','\"\"')
+            out_str = f'{str(date)},"{title}","{content}"\n'
+            fp.write(out_str);
 
 
 # Noun specification   
